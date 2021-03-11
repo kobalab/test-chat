1784a8f9f11b497d276029b3de5c5d7de7322365
Satoshi Kobayashi <koba@yk.rim.or.jp>
2021-03-11 17:01:24 +0800

    ユーザ情報を外部ファイルから取得するようにした

--- src/js/lib/passport.js
+++ src/js/lib/passport.js
@@ -9,16 +9,14 @@ passport.serializeUser((user, done)=>done(null, JSON.stringify(user)));
 passport.deserializeUser((userstr, done)=>done(null, JSON.parse(userstr)));
 
 const local = require('passport-local');
+const users = require('../../../etc/users.json');
 
 passport.use(new local.Strategy(
     { usernameField: 'login',
       passwordField: 'passwd' },
     (login, passwd, done)=>{
-        done(null, {
-            id:   'koba',
-            name: '小林 聡',
-            icon: 'https://kobalab.net/koba.jpg'
-        });
+        let user = users.find(u=>u.id == login);
+        done(null, user);
     }
 ));
 
