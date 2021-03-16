cbaef65054171813609c62ae78525044b3d91f1c
Satoshi Kobayashi <koba@yk.rim.or.jp>
2021-03-16 12:20:27 +0800

    メンバー一覧を表示する機能を追加

--- src/js/client.js
+++ src/js/client.js
@@ -11,14 +11,27 @@ $(function(){
     sock = io();
     sock.onAny(console.log);
     sock.on('hello', (user)=>{
-        if (! user) document.location = './auth/';
-        else {
-            $('#user span').text(user.name);
-            $('#user img').attr('src', user.icon);
-            $('#user').show();
-            sock.emit('join');
+        if (! user) {
+            document.location = './auth/';
+            return;
         }
+        $('#user span').text(user.name);
+        $('#user img').attr('src', user.icon);
+        $('#user').show();
+
         sock.on('disconnect', (reason)=>$('#error').text('接続が切れました'));
+        sock.on('room', (users)=>{
+            $('#member').empty();
+            for (let user of users) {
+                $('<span>').append($('<img>').attr('src', user.icon))
+                           .append($('<span>').text(user.name))
+                           .appendTo($('#member'));
+            }
+            $('#member').append($('<hr>'));
+        });
+        $('#error').empty();
+
+        sock.emit('join');
     });
     sock.on('error', (msg)=>$('#error').text(msg));
 });
